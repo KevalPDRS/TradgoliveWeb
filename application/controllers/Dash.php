997297c03ea318a0ee1ae7d80c147710e3064feb
Jignesh Dabhi <pdrs.jignesh.d@gmail.com>
2024-01-30 17:07:02 +0530

    Dash View Complete With Php Only Design

--- application/controllers/Dash.php
+++ application/controllers/Dash.php
@@ -5,7 +5,7 @@ class Dash extends CI_Controller
 {
     public function index()
     {
-        $this->template->load('master_files/tamplate_master', 'Dash_view');  
+        $this->template->load('master_files/tamplate_master', 'templates/Dash_view');  
     }
     
 }
